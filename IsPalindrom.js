72916bf3d36896bf9605408472471200a09e4dce
Samet <sameterduner1@gmail.com>
2016-09-19 23:33:34 +0300

    first commit

---
+++ IsPalindrom.js
@@ -0,0 +1,29 @@
+// by samet ERDÜNER..
+
+function isPalindrom(number){
+
+	var value = null;
+	number = number.toString();
+
+	for(var i = 0; i < number.length; i++){
+
+		if(number[i]==number[(number.length-1)-i]){
+			value = true;
+			
+		}
+		else{
+			value = false;
+			break;
+		}
+	}
+
+	return value;
+}
+
+var number = 9898989;
+
+if(isPalindrom(number))
+	console.log("it's palindrom");
+else
+	console.log("it isn't palindrom")
+//1001
